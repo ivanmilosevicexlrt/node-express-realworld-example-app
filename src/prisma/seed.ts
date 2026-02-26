385a8e87b1177028e19af4e76e1a0863748e79ae
Ivan Milosevic <imilosevic@cscsw.com>
2026-02-26 23:49:12 +0100

    pipeline refactoring

--- src/prisma/seed.ts
+++ src/prisma/seed.ts
@@ -1,31 +1,66 @@
+import {
+  randEmail,
+  randFullName,
+  randLines,
+  randParagraph,
+  randPassword, randPhrase,
+  randWord
+} from '@ngneat/falso';
 import { PrismaClient } from '@prisma/client';
-import bcrypt from 'bcrypt';
+import { RegisteredUser } from '../app/routes/auth/registered-user.model';
+import { createUser } from '../app/routes/auth/auth.service';
+import { addComment, createArticle } from '../app/routes/article/article.service';
 
 const prisma = new PrismaClient();
 
-async function main() {
-  // Clear existing data to avoid conflicts
-  await prisma.comment.deleteMany();
-  await prisma.article.deleteMany();
-  await prisma.user.deleteMany();
-
-  // Seed a known test user
-  await prisma.user.create({
-    data: {
-      username: 'RealWorld',
-      email: 'realworld@me',
-      password: await bcrypt.hash('password123', 10),
-      bio: null,
-      image: 'https://api.realworld.io/images/smiley-cyrus.jpeg',
-    },
+export const generateUser = async (): Promise<RegisteredUser> =>
+  createUser({
+    username: randFullName(),
+    email: randEmail(),
+    password: randPassword(),
+    image: 'https://api.realworld.io/images/demo-avatar.png',
+    demo: true,
   });
-}
+
+export const generateArticle = async (id: number) =>
+  createArticle(
+    {
+      title: randPhrase(),
+      description: randParagraph(),
+      body: randLines({ length: 10 }).join(' '),
+      tagList: randWord({ length: 4 }),
+    },
+    id,
+  );
+
+export const generateComment = async (id: number, slug: string) =>
+  addComment(randParagraph(), slug, id);
+
+const main = async () => {
+  try {
+    const users = await Promise.all(Array.from({length: 12}, () => generateUser()));
+    users?.map(user => user);
+
+    // eslint-disable-next-line no-restricted-syntax
+    for await (const user of users) {
+      const articles = await Promise.all(Array.from({length: 12}, () => generateArticle(user.id)));
+
+      // eslint-disable-next-line no-restricted-syntax
+      for await (const article of articles) {
+        await Promise.all(users.map(userItem => generateComment(userItem.id, article.slug)));
+      }
+    }
+  } catch (e) {
+    console.error(e);
+
+  }
+};
 
 main()
-  .catch(e => {
-    console.error(e);
-    process.exit(1);
-  })
-  .finally(async () => {
+  .then(async () => {
     await prisma.$disconnect();
+  })
+  .catch(async () => {
+    await prisma.$disconnect();
+    process.exit(1);
   });
